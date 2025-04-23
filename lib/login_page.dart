1d432a060120b35e1aa12421d590cb358943434f
nauvalachid <nauvalachid@gmail.com>
2025-04-23 16:07:22 +0700

    Menambahkan tombol masuk ke halaman masuk

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -48,6 +48,17 @@ class _LoginPageState extends State<LoginPage> {
                   return null;
                 },
               ),
+               ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                  }
+                },
+                child: Text('Login'),
+              ),
             ],
           ),
         ),
