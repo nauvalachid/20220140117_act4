0bef177266f619bb61a55321ea659dd19e5f390c
nauvalachid <nauvalachid@gmail.com>
2025-04-23 16:20:08 +0700

    Menambahkan TextFormField untuk input kata sandi

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -36,6 +36,17 @@ class _RegisterPageState extends State<RegisterPage> {
                   return null;
                 },
               ),
+               TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
             ],
           ),
         ),
