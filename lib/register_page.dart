a67fff2743ac4d321945a44eb96da120063a8ad8
nauvalachid <nauvalachid@gmail.com>
2025-04-23 16:22:25 +0700

    Menambahkan dua tombol baru ke widget RegisterPage: tombol daftar dan tombol masuk.

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -47,6 +47,18 @@ class _RegisterPageState extends State<RegisterPage> {
                   return null;
                 },
               ),
+              ElevatedButton(onPressed: () {}, child: Text('Register')),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                  // Navigator.pop(context);
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const LoginPage()),
+                  // );
+                },
+                child: Text('Sudah punya akun? Login Sekarang!'),
+              ),
             ],
           ),
         ),
