1813bd1eadb766f3091c28eed3b97761e16b42b5
nauvalachid <nauvalachid@gmail.com>
2025-04-23 16:27:13 +0700

    Menambahkan TextFormField ke OrderPage untuk input pengguna

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -22,4 +22,30 @@ class _OrderPageState extends State<OrderPage> {
       totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
     });
   }
+
+   @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(title: Text('Order Page')),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          spacing: 16,
+          children: [
+            TextFormField(
+              controller: makananController,
+              decoration: const InputDecoration(labelText: 'Food Order'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your food order';
+                }
+                return null;
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
