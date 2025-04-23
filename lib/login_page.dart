b0cb8c0ec7fd4ea5013076ae9f1276fdbfc5a81b
nauvalachid <nauvalachid@gmail.com>
2025-04-23 16:08:01 +0700

    Menambahkan TextButton untuk menavigasi ke RegisterPage dari LoginPage

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -59,6 +59,15 @@ class _LoginPageState extends State<LoginPage> {
                 },
                 child: Text('Login'),
               ),
+               TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterPage()),
+                  );
+                },
+                child: Text('Belum punya akun? Daftar'),
+              ),
             ],
           ),
         ),
