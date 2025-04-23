c7881284c13c305b8c0894253f952116b62f4369
nauvalachid <nauvalachid@gmail.com>
2025-04-23 16:16:58 +0700

    Menambahkan implementasi awal widget RegisterPage

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,1 +1,8 @@
 import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
